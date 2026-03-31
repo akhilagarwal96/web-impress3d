1d206926ab5aafa10fcef45411c45a0cd28370d3
Akhil Agarwal <akhilagarwal96@gmail.com>
2026-03-31 15:02:17 +0530

    Add Footer component to CategoryPage

--- src/pages/CategoryPage.jsx
+++ src/pages/CategoryPage.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { useParams, Link } from 'react-router-dom';
 import Header from '../components/Header';
+import Footer from '../components/Footer';
 import { collection, getDocs } from "firebase/firestore";
 import { db } from '../firebase';
 
