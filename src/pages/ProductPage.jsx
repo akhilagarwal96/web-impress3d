0a91cd12b3c782769521056d22ddbe2a34c6da15
Akhil Agarwal <akhilagarwal96@gmail.com>
2026-03-31 15:02:25 +0530

    Add Footer component import to ProductPage

--- src/pages/ProductPage.jsx
+++ src/pages/ProductPage.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import Header from '../components/Header';
+import Footer from '../components/Footer';
 import { doc, getDoc, setDoc } from "firebase/firestore";
 import { db, auth } from '../firebase';
 import { onAuthStateChanged } from 'firebase/auth';
