94ba5a7b9982bdfdd0203a70726ba6b5a1261ad5
Akhil Agarwal <akhilagarwal96@gmail.com>
2026-09-14 11:32:33 +0530

    update - page-admin - app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import CategoryPage from './pages/CategoryPage';
 import ProductPage from './pages/ProductPage';
 import WishlistPage from './pages/WishlistPage';
 import PrivacyPolicy from './pages/PrivacyPolicy';
+import AdminPage from './pages/AdminPage';
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
         <Route path="/product/:productId" element={<ProductPage />} />
         <Route path="/wishlist" element={<WishlistPage />} />
         <Route path="/privacy" element={<PrivacyPolicy />} />
+        <Route path="/admin" element={<AdminPage />} />
       </Routes>
     </Router>
   );
